e3f6941d03a98ab029e402277e48f36592230ad9
ikhovanskiy <khovanskiyi@rambler.ru>
2024-01-12 13:21:33 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import CytoscapeComponent from "react-cytoscapejs";
+import styles from "./page.module.css";
+import { targets, termins } from "@/data/data";
 
 export default function Home() {
+  const layout = { name: "preset" };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div className={styles.title}>
+        <h1 className={styles.h1}>
+          Практика 3 семестр 2023, выполнил Хованский И.А.
+        </h1>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <CytoscapeComponent
+        elements={[...termins, ...targets]}
+        layout={layout}
+        style={{
+          width: "100%",
+          height: "100vh",
+        }}
+        stylesheet={[
+          {
+            selector: "node",
+            style: {
+              color: "rgba(255, 255, 255, 1)",
+              "text-max-width": "200px",
+              "text-valign": "left",
+              "text-halign": "right",
+              "text-wrap": "wrap",
+              content: "data(label)",
+              "background-opacity": 0.8,
+            },
+          },
+          {
+            selector: "edge",
+            style: {
+              width: 2,
+            },
+          },
+        ]}
+      />
     </main>
-  )
+  );
 }
